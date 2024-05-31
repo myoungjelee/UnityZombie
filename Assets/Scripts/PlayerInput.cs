9a27e097aa5d4ec9dcae5d9b9d5d84f904bfeabe
myoungjelee <120433909+myoungjelee@users.noreply.github.com>
2024-05-31 17:27:12 +0900

    캐릭터 이동 및 회전 수정
    This reverts commit 389a0913b8290c05ce70365dab0040fa77d32720.

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -3,13 +3,15 @@
 // 플레이어 캐릭터를 조작하기 위한 사용자 입력을 감지
 // 감지된 입력값을 다른 컴포넌트들이 사용할 수 있도록 제공
 public class PlayerInput : MonoBehaviour {
-    public string moveAxisName = "Vertical"; // 앞뒤 움직임을 위한 입력축 이름
-    public string rotateAxisName = "Horizontal"; // 좌우 회전을 위한 입력축 이름
+    public string moveXAxisName = "Vertical"; // 앞뒤 움직임을 위한 입력축 이름
+    public string moveYAxisName = "Horizontal"; // 좌우 움직임을 위한 입력축 이름
+    public string rotateAxisName = "Mouse X"; // 좌우 회전을 위한 입력축 이름
     public string fireButtonName = "Fire1"; // 발사를 위한 입력 버튼 이름
     public string reloadButtonName = "Reload"; // 재장전을 위한 입력 버튼 이름
 
     // 값 할당은 내부에서만 가능
-    public float move { get; private set; } // 감지된 움직임 입력값
+    public float moveX { get; private set; } // 감지된 움직임 입력값
+    public float moveY { get; private set; } // 감지된 움직임 입력값
     public float rotate { get; private set; } // 감지된 회전 입력값
     public bool fire { get; private set; } // 감지된 발사 입력값
     public bool reload { get; private set; } // 감지된 재장전 입력값
@@ -19,7 +21,8 @@ public class PlayerInput : MonoBehaviour {
         // 게임오버 상태에서는 사용자 입력을 감지하지 않는다
         if (GameManager.instance != null && GameManager.instance.isGameover)
         {
-            move = 0;
+            moveX = 0;
+            moveY = 0;
             rotate = 0;
             fire = false;
             reload = false;
@@ -27,7 +30,8 @@ public class PlayerInput : MonoBehaviour {
         }
 
         // move에 관한 입력 감지
-        move = Input.GetAxis(moveAxisName);
+        moveX = Input.GetAxis(moveXAxisName);
+        moveY = Input.GetAxis(moveYAxisName);
         // rotate에 관한 입력 감지
         rotate = Input.GetAxis(rotateAxisName);
         // fire에 관한 입력 감지
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,45 +1,89 @@
 ﻿using UnityEngine;
 
 // 플레이어 캐릭터를 사용자 입력에 따라 움직이는 스크립트
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
     public float moveSpeed = 5f; // 앞뒤 움직임의 속도
-    public float rotateSpeed = 180f; // 좌우 회전 속도
-
+    public float rotateSpeed = 360f; // 좌우 회전 속도
+    private Transform mapTransform;
+    private Camera mainCamera;
+    public Gun gun;
 
     private PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트
     private Rigidbody playerRigidbody; // 플레이어 캐릭터의 리지드바디
     private Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
 
-    private void Start() 
+    private void Start()
     {
         // 사용할 컴포넌트들의 참조를 가져오기
         playerInput = GetComponent<PlayerInput>();
         playerRigidbody = GetComponent<Rigidbody>();
         playerAnimator = GetComponent<Animator>();
+        mainCamera = Camera.main;
+
+        GameObject leverArt = GameObject.Find("Level Art");
+        if (leverArt != null)
+        {
+            mapTransform = leverArt.transform;
+        }
+        else
+        {
+            Debug.LogWarning("Level Art 오브젝트를 찾을 수 없습니다.");
+        }
     }
 
     // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
-    private void FixedUpdate() 
+    private void FixedUpdate()
     {
         // 물리 갱신 주기마다 움직임, 회전, 애니메이션 처리 실행
         Rotate();
         Move();
 
-        playerAnimator.SetFloat("Move", playerInput.move);
+        float move = playerInput.moveX == 0 ? playerInput.moveY : playerInput.moveX;
+        playerAnimator.SetFloat("Move", move);
     }
 
     // 입력값에 따라 캐릭터를 앞뒤로 움직임
-    private void Move() 
+    private void Move()
     {
-        Vector3 moveDistance = playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
+        //Vector3 moveDistance = playerInput.moveX * transform.forward * moveSpeed * Time.deltaTime;
+
+        //Vector3 moveDistance = (playerInput.moveX * transform.forward + playerInput.moveY * transform.right).normalized * moveSpeed * Time.deltaTime; 
+
+        //Vector3 moveDistance = new Vector3(playerInput.moveY, 0f, playerInput.moveX).normalized * moveSpeed * Time.deltaTime;
+
+        //Vector3 moveX = playerInput.moveX * mapTransform.transform.forward * moveSpeed * Time.deltaTime;
+        //Vector3 moveY = playerInput.moveY * mapTransform.transform.right * moveSpeed * Time.deltaTime;
+        //Vector3 moveDistance = moveX + moveY;
+
+        Vector3 cameraForward = mainCamera.transform.forward;
+        cameraForward.y = 0f;
+        Vector3 cameraRight = mainCamera.transform.right;
+        cameraRight.y = 0f;
+        Vector3 moveX = playerInput.moveX * cameraForward.normalized;
+        Vector3 moveY = playerInput.moveY * cameraRight.normalized;
+        Vector3 moveDistance = (moveX + moveY).normalized * moveSpeed * Time.deltaTime;
         playerRigidbody.MovePosition(playerRigidbody.position + moveDistance);
     }
 
     // 입력값에 따라 캐릭터를 좌우로 회전
-    private void Rotate() 
+    private void Rotate()
     {
         float turn = playerInput.rotate * rotateSpeed * Time.deltaTime;
         playerRigidbody.rotation = playerRigidbody.rotation * Quaternion.Euler(0, turn, 0);
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, new Vector3(0, gun.transform.position.y, 0));
+//plane.Translate(0, gun.transform.position.y, 0);
+        if(plane.Raycast(ray, out float distance))
+        {
+            Vector3 target = ray.GetPoint(distance);
+            Vector3 direction = (target - transform.position).normalized;
+            direction.y = 0f;
+            Quaternion rotation = Quaternion.LookRotation(direction);
+
+            playerRigidbody.MoveRotation(rotation);
+        }
 
 
         ////마우스 움직임 따라 회전
